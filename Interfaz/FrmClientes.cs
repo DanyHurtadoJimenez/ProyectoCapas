fe702c51b0c0a03ee6b59de5f721ead47050bb9c
DanyHurtadoJimenez <dannyjosue822@gmail.com>
2021-10-22 08:09:20 -0600

    capas y formulario

---
+++ Interfaz/FrmClientes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Interfaz
+{
+    public partial class FrmClientes : Form
+    {
+        public FrmClientes()
+        {
+            InitializeComponent();
+        }
+        //metodo de limpiar
+        private void Limpiar() {
+
+            TxtIdCliente.Text = string.Empty;
+            TxtNombre.Text = string.Empty;
+            TxtTelefono.Text = string.Empty;
+            TxtDireccion.Text = string.Empty;
+            TxtNombre.Focus();
+
+        }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
